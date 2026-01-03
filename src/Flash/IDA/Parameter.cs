3ce32622846569b367c18d810f76036eab5a3f34
Tom David Müller <tom.mueller@beachouse.de>
2026-01-03 12:39:03 +0100

    add tsv based inclusion list

--- src/Flash/IDA/Parameter.cs
+++ src/Flash/IDA/Parameter.cs
@@ -46,6 +46,10 @@ namespace Flash.IDA
         public bool ConsiderAllChargeStates { set; get; }
         public int HCDEnergy { set; get; }
 
+        public bool StrictInclusion { set; get; }
+        public string InclusionList { set; get; }
+        public string PtmList { set; get; }
+
         /// <summary>
         /// Complete constructor
         /// </summary>
@@ -63,7 +67,8 @@ namespace Flash.IDA
         public IDAParameters(double[] tolerances = null, int maxMs2CountPerMs1 = 5, double qScoreThreshold = -1, double rtWindow = 5, int minCharge = 1, int maxCharge = 100,
                              double minMass = 50, double maxMass = 100000, List<string> targetLogs = null, int targetMode = 0, double[] cvvalues = null, double cycletime = 180, bool usecvqscore = true,
                              int MaxCVSkip_ = 0, int MassThreshold_ = 15, double tqScoreThreshold = 0.9, double quantReporterMZTol_ = 0, double quantFoldChangeThreshold_ = 0, bool quantOnlyOneCondition_ = false,
-                             bool UseIDScore_ = false, bool ConsiderAllChargeStates_ = false, int HCDEnergy_ = 29)
+                             bool UseIDScore_ = false, bool ConsiderAllChargeStates_ = false, int HCDEnergy_ = 29,
+                             bool strictInclusion = false, string inclusionList = null, string ptmList = null)
         {
             Tolerances = tolerances ?? new double[] { 10, 10 };
             CVValues = cvvalues ?? new double[] { 0.0, -40.0, -50.0, -60.0 };
@@ -87,6 +92,9 @@ namespace Flash.IDA
             UseIDScore = UseIDScore_;
             ConsiderAllChargeStates = ConsiderAllChargeStates_;
             HCDEnergy = HCDEnergy_;
+            StrictInclusion = strictInclusion;
+            InclusionList = inclusionList;
+            PtmList = ptmList;
         }
 
         /// <summary>
@@ -103,13 +111,25 @@ namespace Flash.IDA
         /// <returns></returns>
         public string ToFLASHDeconvInput()
         {
-            var ret = String.Format("max_mass_count {0} score_threshold {1} min_charge {2} max_charge {3} min_mass {4} max_mass {5} RT_window {6} tol {7} tqscore_threshold {8} target_mode {9} IDScore {10} AllCharges {11} HCDEnergy {12} ",
-                MaxMs2CountPerMs1, QScoreThreshold, MinCharge, MaxCharge, MinMass, MaxMass, RTWindow, String.Join(" ", Tolerances), TQScoreThreshold, TargetMode, UseIDScore ? 1 : 0, ConsiderAllChargeStates ? 1 : 0, HCDEnergy);
+            var ret = String.Format("max_mass_count {0} score_threshold {1} min_charge {2} max_charge {3} min_mass {4} max_mass {5} RT_window {6} tol {7} tqscore_threshold {8} target_mode {9} IDScore {10} AllCharges {11} HCDEnergy {12} strict_inclusion {13} ",
+                MaxMs2CountPerMs1, QScoreThreshold, MinCharge, MaxCharge, MinMass, MaxMass, RTWindow, String.Join(" ", Tolerances), TQScoreThreshold, TargetMode, UseIDScore ? 1 : 0, ConsiderAllChargeStates ? 1 : 0, HCDEnergy, StrictInclusion ? 1 : 0);
 
             foreach(var f in TargetLogs)
             {
                 ret += f + " ";
             }
+
+            // PTM list must come before inclusion list (file extension detection order)
+            if (!String.IsNullOrEmpty(PtmList))
+            {
+                ret += PtmList + " ";
+            }
+
+            if (!String.IsNullOrEmpty(InclusionList))
+            {
+                ret += InclusionList + " ";
+            }
+
             return ret;
         }
 
